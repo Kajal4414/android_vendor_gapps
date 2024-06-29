8aa9a4c649df56e7af0a798b2aaea2d9a4fefcf2
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-29 10:39:24 +0000

    init gapps for a13
    Signed-off-by: GitHub <noreply@github.com>

---
+++ common.mk
@@ -0,0 +1,47 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/gapps
+
+WITH_GMS := true
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/gapps/product/etc/,$(TARGET_COPY_OUT_PRODUCT)/etc) \
+    $(call find-copy-subdir-files,*,vendor/gapps/system_ext/etc/,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc)
+
+PRODUCT_COPY_FILES += \
+    vendor/gapps/product/framework/com.google.android.dialer.support.jar:$(TARGET_COPY_OUT_PRODUCT)/framework/com.google.android.dialer.support.jar \
+    vendor/gapps/product/lib/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_PRODUCT)/lib/libjni_latinimegoogle.so \
+    vendor/gapps/product/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libjni_latinimegoogle.so
+
+# Product
+PRODUCT_PACKAGES += \
+    GoogleCalendarSyncAdapter \
+    GoogleContactsSyncAdapter \
+    SoundPickerPrebuilt \
+    SpeechServicesByGoogle \
+    AndroidAutoStub \
+    GmsCore \
+    GooglePartnerSetup \
+    GoogleRestore \
+    Phonesky \
+    Velvet
+
+# System_ext
+PRODUCT_PACKAGES += \
+    GoogleServicesFramework \
+    SetupWizard
+
+# Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.opa.eligible_device=true \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.theme=glif_v3_light
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
