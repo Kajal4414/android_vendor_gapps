21123c9814c9bf2aeebb7730fe9712ad72af86f3
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-29 11:21:34 +0000

    add GmsCore in parts
    Signed-off-by: GitHub <noreply@github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
-cat PrebuiltGmsCoreParts/PrebuiltGmsCore* > product/priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk
+# product/priv-app/GmsCore/GmsCore.apk sha256: a2de13237e27901b83cb5ed042129d2cbf533634679bf63b8ff0c05ce043f397
+cat temp/GmsCorePart* > product/priv-app/GmsCore/GmsCore.apk && rm -rf temp
