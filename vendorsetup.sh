53adc2656147ee39c66b98b2dd70d7031949d965
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-29 17:20:35 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,16 @@
-# product/priv-app/GmsCore/GmsCore.apk sha256: a2de13237e27901b83cb5ed042129d2cbf533634679bf63b8ff0c05ce043f397
-cat temp/GmsCorePart* > product/priv-app/GmsCore/GmsCore.apk && rm -rf temp
+#!/bin/bash
+
+EXPECTED_SHA256="a2de13237e27901b83cb5ed042129d2cbf533634679bf63b8ff0c05ce043f397"
+
+# Concatenate GmsCorePart* files into GmsCore.apk
+cat temp/GmsCorePart* >product/priv-app/GmsCore/GmsCore.apk || { echo "Error: Concatenation failed."; }
+
+# Calculate SHA256 hash after concatenation
+NEW_SHA256=$(sha256sum product/priv-app/GmsCore/GmsCore.apk | awk '{print $1}')
+
+# Compare hashes
+if [ "$EXPECTED_SHA256" == "$NEW_SHA256" ]; then
+    rm -rf temp && echo "Temp directory deleted."
+else
+    echo "Error: SHA256 hash does not match!"
+fi
